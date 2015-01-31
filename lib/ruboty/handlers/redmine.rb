762756b6e70b509e63232715d86f67839cf4bed3
Ryota Arai <ryota.arai@gmail.com>
2015-01-31 23:02:53 +0900

    `register redmine alias ...`

--- lib/ruboty/handlers/redmine.rb
+++ lib/ruboty/handlers/redmine.rb
@@ -1,6 +1,8 @@
 module Ruboty
   module Handlers
     class Redmine < Base
+      NAMESPACE = 'redmine'
+
       env :REDMINE_URL, 'Redmine url (e.g. http://your-redmine)', optional: false
       env :REDMINE_API_KEY, 'Redmine REST API key', optional: false
       env :REDMINE_BASIC_AUTH_USER, 'Basic Auth User', optional: true
@@ -12,10 +14,22 @@ module Ruboty
         description: 'Create a new issue'
       )
 
+      on(
+        /register redmine alias "(?<name>[^"]+)" ("(?<expand_to>[^"]+)"|'(?<expand_to>[^']+)')/,
+        name: 'register_alias',
+        description: 'Register an alias'
+      )
+
       def create_issue(message)
-        words = message[:rest].scan(/("([^"]+)"|([^ ]+))/).map {|v| v[1] || v[2] }
+        words = parse_arg(message[:rest])
         req = {}
         req[:subject] = words.shift
+
+        if words.size == 1
+          expand_to = alias_for(words.first)
+          words = parse_arg(expand_to) if expand_to
+        end
+
         words.each_with_index do |word, i|
           next if i == 0
 
@@ -63,6 +77,11 @@ module Ruboty
         message.reply("Issue created: #{redmine.url_for_issue(issue)}")
       end
 
+      def register_alias(message)
+        aliases = (robot.brain.data[NAMESPACE][:aliases] ||= {})
+        aliases[message[:name]] = message[:expand_to]
+      end
+
       private
 
       def redmine
@@ -72,6 +91,18 @@ module Ruboty
           basic_auth_user: ENV['REDMINE_BASIC_AUTH_USER'],
           basic_auth_password: ENV['REDMINE_BASIC_AUTH_PASSWORD'],
         )
+      end
+
+      def alias_for(name)
+        aliases = robot.brain.data[NAMESPACE][:aliases] || {}
+        aliases[name]
+      end
+
+      def parse_arg(text)
+        text.scan(/("([^"]+)"|'([^']+)'|([^ ]+))/).map do |v|
+          v.shift
+          v.find {|itself| itself }
+        end
       end
     end
   end
